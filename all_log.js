ded0b5b961e31b065f6a1df1fa97babcd2b0d52e
Rockpods <90806701+Rockpods1@users.noreply.github.com>
2022-04-08 21:50:26 -0700

    Add files via upload

---
+++ all_log.js
@@ -0,0 +1,4 @@
+console.log('%c%s', 'color: white; font-size: 100px;text-shadow: 0px 0px 20px black;', 'Hold Up!');
+console.log('%c%s', 'color: white; font-size: 25px;text-shadow: 0px 0px 5px red;', "peepee.");
+console.log('%c%s', 'color: white; font-size: 35px;text-shadow: 0px 0px 10px darkred;', 'poopoo.');
+console.log('%c%s', 'color: white; font-size: 25px;text-shadow: 0px 0px 5px skyblue;', 'hehehehe');
